21d3422262f17279410a7d3581aa40ab65bc6144
Mario Romero <marioromero.ttp@gmail.com>
2025-11-20 22:51:15 -0300

    Inicio y libs

---
+++ obi-client-front/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div class="text-center p-6 bg-white rounded-lg shadow-xl">
+      <h1 class="text-3xl font-bold text-blue-600 mb-2">
+        OBI Client Agent
+      </h1>
+      <p class="text-gray-600">Entorno Frontend Preparado</p>
+    </div>
+  </div>
+</template>
